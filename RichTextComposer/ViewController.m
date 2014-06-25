cfbc908436701363888e7480d73a7804da4a495c
Shire Jiang <shirejiang@gmail.com>
2014-06-25 21:27:04 +0800

    init project

--- RichTextComposer/ViewController.m
+++ RichTextComposer/ViewController.m
@@ -9,21 +9,80 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
 
+- (IBAction)selectPhoto:(UIBarButtonItem *)sender;
+- (IBAction)printHTML:(UIBarButtonItem *)sender;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self loadWebView];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Helper
+
+- (void)loadWebView {
+    [self.webView setDelegate:self];
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *indexFileURL = [bundle URLForResource:@"composer" withExtension:@"html"];
+
+    [self.webView setKeyboardDisplayRequiresUserAction:NO];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:indexFileURL]];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSString *inputPlaceholder = NSLocalizedString(@"Title", @"Title");
+    NSString *contentPlaceholder = NSLocalizedString(@"Content", @"Content");
+    NSString *script = [NSString stringWithFormat:@"window.initPlaceholder('%@', '%@')", inputPlaceholder, contentPlaceholder];
+    [webView stringByEvaluatingJavaScriptFromString:script];
+}
+
+- (IBAction)selectPhoto:(UIBarButtonItem *)sender {
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePickerController.delegate = self;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+}
+
+- (IBAction)printHTML:(UIBarButtonItem *)sender {
+    NSString *title = [self.webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('title-input').value"];
+    NSString *html = [self.webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('content').innerHTML"];
+    NSString *script = [self.webView stringByEvaluatingJavaScriptFromString:@"window.alertHtml()"];
+    [self.webView stringByEvaluatingJavaScriptFromString:script];
+    NSLog(@"Title: %@", title);
+    NSLog(@"Inner HTML: %@", html);
+}
+
+- (NSString *)stringFromDate:(NSDate *)date{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd-HH-mm-ss"];
+    NSString *destDateString = [dateFormatter stringFromDate:date];
+    return destDateString;
+}
+
+#pragma mark - ImagePickerController Delegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    // Obtain the path to save to
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSDate *now = [NSDate date];
+    NSString *imageName = [NSString stringWithFormat:@"photo%@.jpg", [self stringFromDate:now]];
+    NSString *imagePath = [documentsDirectory stringByAppendingPathComponent:imageName];
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    if ([mediaType isEqualToString:@"public.image"]) {
+        UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        NSData *imageData = UIImageJPEGRepresentation(image, 1);
+        [imageData writeToFile:imagePath atomically:YES];
+    }
+
+    NSString *script = [NSString stringWithFormat:@"window.insertImage('%@', '%@')", imageName, imagePath];
+    [self.webView stringByEvaluatingJavaScriptFromString:script];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
